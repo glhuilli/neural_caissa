fa0864be2a44b89643829d291b0e70aa531e48ac
Gaston L Huillier <glhuilli@gmail.com>
2020-11-27 16:55:31 -0800

    Improved README and better data generations script

--- README.md
+++ README.md
@@ -36,3 +36,12 @@ How does it work
 
 TBA.
 
+
+List of TODOs 
+----
+1. Speed up and/or distribute the serialization strategy for the Tensor representation 12x8x8 of the Board's state. (currently it's about ~ 6it/s), which it's slow if you consider that the training data is about 10M of games (~38M iterations if you consider that in average there's 38 moves per game [[source](https://chess.stackexchange.com/questions/2506/what-is-the-average-length-of-a-game-of-chess#:~:text=The%20average%20number%20of%20moves%20per%20game%20is%20around%2038.)]). This would take ~73 days to process.   
+2. Improve space search strategy when running the Minimax algorithm (note that it's already including the alpha-beta pruning strategy) (@geohot in Twitchchess mentions using [Beam search](https://medium.com/@dhartidhami/beam-search-in-seq2seq-model-7606d55b21a5) for this).
+3. Implement a `MuZero` style strategy to learn how to play chess from the game itself, instead of relying on the minimax backtracking algorithm to find the best next move (maybe something like [this](https://medium.com/applied-data-science/how-to-build-your-own-muzero-in-python-f77d5718061a))   
+4. Improve the `ChessConvNet` structure so it's better suited for Chess. Currently is very simple stack of ConvNets, and most likely not using the right representation. I think that it can be particularly improved by using a different loss function (I personally like the loss function proposed by @erikbern in his [deep-pink implementation](https://github.com/erikbern/deep-pink)).  
+5. Improve the serialization strategy as the current one is extremely simple.
+6. Update the training script so it can be run in [Google Collab](https://pytorch.org/tutorials/beginner/colab.html) (or somewhere that it can use the GPU for training). 
--- neural_caissa/data/generate.py
+++ neural_caissa/data/generate.py
@@ -1,22 +1,22 @@
-import os
 import random
 
+import click
 import chess.pgn
 import numpy as np
+from tqdm import tqdm
 
 from neural_caissa.board.state import State
+
 
 _OUTCOME = {'1/2-1/2': 0, '0-1': -1, '1-0': 1}
 
 
-def _generate_dataset(data_path, samples=None):
+def _generate_dataset(data_file_path, samples=None):
     X_origin, X_move, X_random, Y = [], [], [], []
     games_counter = 0
-    for fn in os.listdir(data_path):
-        pgn = open(os.path.join(data_path, fn))
-        while 1:
-            if samples and games_counter > samples:
-                break
+    progress_bar = tqdm(total=games_counter + 1)
+    with open(data_file_path, 'r') as pgn:
+        while games_counter < samples:
             game = chess.pgn.read_game(pgn)
             if game is None:
                 break
@@ -46,7 +46,8 @@ def _generate_dataset(data_path, samples=None):
                 X_random.append(x_random)
                 Y.append(_y)
             games_counter += 1
-
+            progress_bar.update(1)
+    progress_bar.close()
     X_origin = np.array(X_origin)
     X_move = np.array(X_move)
     X_random = np.array(X_random)
@@ -54,7 +55,16 @@ def _generate_dataset(data_path, samples=None):
     return X_origin, X_move, X_random, Y
 
 
-if __name__ == "__main__":
-    X_origin, X_move, X_random, Y = _generate_dataset('data/raw_data', 100_000)
-    np.savez('data/serialized_data/dataset_1k.npz', X_origin, X_move, X_random, Y)
+@click.command()
+@click.option('--input_data_file',
+              default='data/raw_data/1k_caissadb_data.pgn',
+              help='Input PGN data file.')
+@click.option('--output_data', default='data/serialized_data/dataset_1k.npz', help='Output serialized data file.')
+def main(input_data_file, output_data):
+    X_origin, X_move, X_random, Y = _generate_dataset(input_data_file, 100_000)
+    np.savez(output_data, X_origin, X_move, X_random, Y)
     print("saved", X_origin.shape, X_move.shape, X_random.shape, Y.shape)
+
+
+if __name__ == "__main__":
+    main()

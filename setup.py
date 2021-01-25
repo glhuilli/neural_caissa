af9bc73c69b2a8aaee5461f98b1db89b4e8fcac6
Gaston L'Huillier <glhuilli@gmail.com>
2021-01-24 19:58:55 -0800

    Caissa vs Caissa basic code and script (#5)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
       name='neural_caissa',
-      version='0.0.1',
+      version='0.0.2',
       description='Python package for Chess playing.',
       url='https://github.com/glhuilli/ah_heh_drehs',
       author="Gaston L'Huillier",
